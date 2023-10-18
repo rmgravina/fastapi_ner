84f3b3f8e7a23e598846bc60f0c1c438d39576cb
Rogério Machado Gravina <rmgravina@gmail.com>
2023-10-18 13:58:03 -0300

    v1
    work version only for .txt

---
+++ app.py
@@ -0,0 +1,37 @@
+import spacy
+from fastapi import FastAPI, File, UploadFile
+import uvicorn
+from secrets import token_hex
+
+# Carregar o modelo NER ('lener' é um modelo customizado pela equipe)
+NER = spacy.load('lener')
+
+# Criar o App FastAPI
+app = FastAPI(title="E-Notifica API", description="Reconhecimento de Entidades Nomeadas - NER")
+
+# Criar função que identifica as entidades nomeadas
+@app.post("/")
+def ner_pessoas(file_uploaded: UploadFile = File(...)):
+
+    file_ext = file_uploaded.filename.split(".").pop()
+    if file_ext not in ["pdf", "docx", "doc", "txt"]:
+        return "Formato de arquivo não suportado"
+
+    conteudo = str(file_uploaded.file.read().decode('utf-8').replace("\n", " ").replace("\r", " ").replace("\t", " ").replace("__________________________", ""))
+
+    document = NER(conteudo)
+
+    ents_unique = set()
+
+    for ent in document.ents:
+        if ent.label_ == 'PER':
+            ents_unique.add(ent.text)
+
+    dict_final = {"pessoas": list(ents_unique)}
+
+    return dict_final
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
